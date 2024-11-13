79fc4d0bdc78c2573bdf81677b1aaec85a0ac7eb
mael-bomane <me@ez-krk.dev>
2024-11-13 09:40:21 +0100

    added @/lib/program/proposalNew.ts

--- app/lib/program/proposalNew.ts
+++ app/lib/program/proposalNew.ts
@@ -1,56 +1,49 @@
 
 import { BN } from "@coral-xyz/anchor";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 import { program } from "@/constants/program";
+import { ProposalChoice } from "@/types";
 
-export const asrDepositIx = async (
-  creator: PublicKey,
-  mint: PublicKey,
-  signerAta: PublicKey,
-  amount: BN,
+export const proposalNewIx = async (
+  owner: PublicKey,
+  lock: PublicKey,
+  title: string,
+  choices: ProposalChoice[],
+  id: BN
 ) => {
-  console.log("creator", creator.toString());
-  console.log("mint", mint.toString());
-  console.log("signerAta", signerAta.toString());
-  console.log("amount", amount.toNumber());
+  console.log("owner : ", owner.toString());
+  console.log("lock : ", lock.toString());
+  console.log("title : ", title);
+  console.log("choices : ", choices);
 
   const analytics = PublicKey.findProgramAddressSync(
     [Buffer.from("analytics")],
     program.programId
   )[0];
 
-  const auth = PublicKey.findProgramAddressSync(
-    [Buffer.from("auth"), analytics.toBuffer()],
+  const user = PublicKey.findProgramAddressSync(
+    // seeds = [b"user", lock.key().as_ref(), signer.key().as_ref()]
+    [Buffer.from("user"), lock.toBytes(), owner.toBytes()],
     program.programId
   )[0];
 
-  const lock = PublicKey.findProgramAddressSync(
-    // seeds = [b"lock", creator.key().as_ref(), mint.key().as_ref()]
-    [Buffer.from("lock"), creator.toBytes(), mint.toBytes()],
+  const poll = PublicKey.findProgramAddressSync(
+    // seeds = [b"poll", lock.key().as_ref(), (locker.polls + 1).to_le_bytes().as_ref()]
+    [Buffer.from("poll"), lock.toBytes(), id.toArrayLike(Buffer, 'le', 8)],
     program.programId
   )[0];
 
-  const vault = PublicKey.findProgramAddressSync(
-    // seeds = [b"vault", lock.key().as_ref(), mint.key().as_ref()]
-    [Buffer.from("vault"), lock.toBytes(), mint.toBytes()],
-    program.programId
-  )[0];
 
   // @ts-ignore
   return await program.methods.pollNew(amount)
     .accountsStrict({
-      creator,
-      signerAta,
-      mint,
+      owner,
+      poll,
       lock,
-      vault,
-      auth,
+      user,
       analytics,
       systemProgram: SystemProgram.programId,
-      tokenProgram: TOKEN_PROGRAM_ID,
-      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID
     })
     .instruction()
 }
