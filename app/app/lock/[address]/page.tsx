3d634a9e5aef45555fc6e3445b7bb5d41f84c0c1
mael-bomane <me@ez-krk.dev>
2024-11-14 14:01:01 +0100

    fixed getSEOTags in @/app/lock/[address]/page.tsx

--- app/app/lock/[address]/page.tsx
+++ app/app/lock/[address]/page.tsx
@@ -1,11 +1,4 @@
-import { Proposals } from "@/components/Proposals";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { getSEOTags } from "@/lib/seo";
-import { createClient } from "@/lib/supabase/client";
-import Image from "next/image";
-import Link from "next/link";
-import { IoDiamond, IoWallet } from "react-icons/io5";
-import { Input } from "@/components/ui/input";
 import { Monolith } from "@/components/Monolith";
 
 
@@ -15,19 +8,19 @@ export async function generateMetadata({
   params: { address: string };
 }) {
 
-
+  // @ts-ignore
   return getSEOTags({
-    title: `/monolith/${params.address}`,
-    description: `/monolith/${params.address}`,
-    canonicalUrlRelative: `/monolith/${params.address}`,
+    title: `/lock/${params.address}`,
+    description: `/lock/${params.address}`,
+    canonicalUrlRelative: `/lock/${params.address}`,
     extraTags: {
       openGraph: {
-        title: `/monolith/${params.address}`,
-        description: `/monolith/${params.address}`,
-        url: `/monolith/${params.address}`,
+        title: `/lock/${params.address}`,
+        description: `/lock/${params.address}`,
+        url: `/lock/${params.address}`,
         images: [
           {
-            url: `/monolith/${params.address}.png`,
+            url: `/lock/${params.address}.png`,
             width: 1200,
             height: 660,
           },
