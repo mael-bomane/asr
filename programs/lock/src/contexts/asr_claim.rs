6a43061bf39e9c4429e9024c30a5b417ee854074
mael-bomane <me@ez-krk.dev>
2024-11-19 19:31:54 +0100

    now if asr claim is lock mint, add a deposit to user and transfer asr tokens to his vault

--- programs/lock/src/contexts/asr_claim.rs
+++ programs/lock/src/contexts/asr_claim.rs
@@ -30,9 +30,17 @@ pub struct ASRClaim<'info> {
         realloc::payer = owner,
         seeds = [b"user", lock.key().as_ref(), owner.key().as_ref()],
         bump = user.bump,
-        constraint = !user.claims.iter().any(|i| i.mint == mint.key()) @ ErrorCode::UserAlreadyClaimedThis
+        constraint = !user.claims.iter().any(|i| i.mint == mint.key() && i.season == lock.seasons.len() as u8 - 2) @ ErrorCode::UserAlreadyClaimedThis
     )]
     pub user: Box<Account<'info, User>>,
+    #[account(
+        mut,
+        seeds = [b"vault", lock.key().as_ref(), owner.key().as_ref()],
+        token::mint = mint,
+        token::authority = auth,
+        bump
+    )]
+    pub user_vault: Box<Account<'info, TokenAccount>>,
     #[account(
         mut,
         associated_token::mint = mint,
@@ -63,7 +71,7 @@ impl<'info> ASRClaim<'info> {
     pub fn asr_claim(&mut self) -> Result<()> {
         let lock = &mut self.lock;
 
-        let season = &lock.seasons[lock.seasons.len() as usize - 2];
+        let season = &lock.seasons.clone()[lock.seasons.len() as usize - 2];
         let asr = season
             .asr
             .clone()
@@ -83,31 +91,66 @@ impl<'info> ASRClaim<'info> {
 
         let amount = user_ratio.checked_mul(total_tokens).unwrap().div_ceil(100);
 
-        let accounts = Transfer {
-            from: self.vault.to_account_info(),
-            to: self.signer_ata.to_account_info(),
-            authority: self.auth.to_account_info(),
-        };
+        // if asr claim is locker mint, we transfer tokens to user vault + create a deposit
+        if self.mint.key() == lock.config.mint {
+            let accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.user_vault.to_account_info(),
+                authority: self.auth.to_account_info(),
+            };
 
-        let seeds = &[
-            b"auth",
-            self.analytics.to_account_info().key.as_ref(),
-            &[self.analytics.auth_bump],
-        ];
+            let seeds = &[
+                b"auth",
+                self.analytics.to_account_info().key.as_ref(),
+                &[self.analytics.auth_bump],
+            ];
 
-        let signer_seeds = &[&seeds[..]];
+            let signer_seeds = &[&seeds[..]];
 
-        let cpi = CpiContext::new_with_signer(
-            self.token_program.to_account_info(),
-            accounts,
-            signer_seeds,
-        );
-        transfer(cpi, amount)?;
+            let cpi = CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                accounts,
+                signer_seeds,
+            );
+
+            transfer(cpi, amount)?;
+
+            user.deposits.push(Deposit {
+                amount,
+                deactivating: false,
+                deactivation_start: None,
+                created_at: Clock::get()?.unix_timestamp,
+                expires_at: Clock::get()?.unix_timestamp + lock.config.lock_duration,
+            });
+
+            lock.total_deposits += amount;
+        } else {
+            let accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.signer_ata.to_account_info(),
+                authority: self.auth.to_account_info(),
+            };
+
+            let seeds = &[
+                b"auth",
+                self.analytics.to_account_info().key.as_ref(),
+                &[self.analytics.auth_bump],
+            ];
+
+            let signer_seeds = &[&seeds[..]];
+
+            let cpi = CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                accounts,
+                signer_seeds,
+            );
+            transfer(cpi, amount)?;
+        }
 
         user.claims.push(Claim {
             season: season.season,
             mint: self.mint.key(),
-            amount: amount as u64,
+            amount,
             created_at: Clock::get()?.unix_timestamp,
         });
 
